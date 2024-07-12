8130314025429b3b6c0acc4e8661b2c115bbbe55
Aarav 238 <aarav8090shukla@gmail.com>
2024-07-12 12:29:58 +0530

    stream intialize

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -4,12 +4,22 @@ import { useSocket } from "../context/SocketProvider";
 const Room = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
-
+  const [mystream , setStream] = useState(null)
   const userJoined = useCallback(({ email, id }) => {
     console.log("user connected ", email, id);
     setRemoteSocketId(id);
   }, []);
 
+
+  const handleCallUser = useCallback(
+    async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
+
+      setStream(stream)
+    },
+    [],
+  )
+  
   useEffect(() => {
     socket.on("user:joined", userJoined);
 
@@ -22,6 +32,8 @@ const Room = () => {
     <div>
       <h1>Room</h1>
       <h4>{remoteSocketId ? "connected": 'No Connection'}</h4>
+      {remoteSocketId && (<button onClick={}>Call</button>)}
+      
     </div>
   );
 };
