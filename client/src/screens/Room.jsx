04b3adb97a983a12d1d2dc9359df2218884f4c2a
Aarav 238 <aarav8090shukla@gmail.com>
2024-07-12 12:20:50 +0530

    vc room

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,5 +1,22 @@
+import { useCallback, useEffect } from "react";
+import { useSocket } from "../context/SocketProvider"
 
 const Room = () => {
+
+    const socket = useSocket();
+
+    const userJoined = useCallback(({email , id}) => {
+        console.log(email , id);
+    }, [])
+
+    useEffect(() => {
+        socket.on("user:joined", userJoined)
+
+        return () => {
+            socket.off("user:joined", userJoined)
+        }
+    } , [socket,userJoined])
+
   return (
     <div><h1>Room</h1></div>
   )
